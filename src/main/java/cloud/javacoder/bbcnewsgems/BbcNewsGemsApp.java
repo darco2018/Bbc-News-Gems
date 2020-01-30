73f34c5453949be3c2cbdb843fb4606def3862fd
darco2018 <newsecond@outlook.com>
2020-01-30 14:57:55 +0100

    Remove unneeded deps & dirs

---
+++ src/main/java/cloud/javacoder/bbcnewsgems/BbcNewsGemsApp.java
@@ -0,0 +1,31 @@
+package cloud.javacoder.bbcnewsgems;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class BbcNewsGemsApp {
+
+	@Value("${spring.message}") //  not working
+	private String activeProfile;
+
+	public static void main(String[] args) {
+
+		ApplicationContext appContext = SpringApplication.run(BbcNewsGemsApp.class, args);
+
+		//System.out.println(activeProfile );
+		System.out.printf("" +
+				"6. LIVE RELOAD:" +
+				"   b) Build project automatically (only works when not running or debugging)\n" +
+				"   c) LIVE RELOAD extension enabled in Browser! (check Chrome Dev Tools >> Network)\n" +
+				"   NOTE: DOESNT WORK IN FIREFOX or .jar. Ctrl + S triggers automake after some seconds!!! \n" +
+				"7. HAL-EXPLORER: localhost:8080 redirects  to http://localhost:8080/explorer/index.html#/\n" +
+				"8. ACTUATOR: localhost:8080/actuator/\n" +
+				"9. H2 console available at '/h2-console'");
+
+
+	}
+
+}
--- src/main/resources/db/migration/V1.0.1__Insert_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO GITHUB_PROJECT(GITHUB_USER,PROJECT_NAME) values ('spring-projects', 'spring-boot');
-INSERT INTO GITHUB_PROJECT(GITHUB_USER,PROJECT_NAME) values ('spring-io', 'initializr');
-INSERT INTO GITHUB_PROJECT(GITHUB_USER,PROJECT_NAME) values ('spring-io', 'sagan');
