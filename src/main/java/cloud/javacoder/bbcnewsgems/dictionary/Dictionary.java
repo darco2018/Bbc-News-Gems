07f7212227c59b1cce810bb819057145837bffee
darco2018 <newsecond@outlook.com>
2020-02-05 18:56:58 +0100

    add containsWord() in Dictionary

--- src/main/java/cloud/javacoder/bbcnewsgems/dictionary/Dictionary.java
+++ src/main/java/cloud/javacoder/bbcnewsgems/dictionary/Dictionary.java
@@ -1,17 +1,36 @@
 package cloud.javacoder.bbcnewsgems.dictionary;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Data
 public class Dictionary {
 
     private List<DictionaryEntry> entries = new ArrayList<>();
+    private Map<String, DictionaryEntry> dictionary = new HashMap<>();
 
     public void addEntry(DictionaryEntry dictionaryEntry) {
-
+        log.info("----------->> Adding dictionaryEntry for word: " + dictionaryEntry.getWord() );
         this.entries.add(dictionaryEntry);
+        this.dictionary.put(dictionaryEntry.getWord(), dictionaryEntry);
+    }
+
+    public int containsWord(String word){
+        log.info("Contains word: " + word + " ?");
+        DictionaryEntry entry = this.dictionary.get(word);
+        log.info("DictionaryEntry entry " + entry);
+
+        if(entry == null){
+            return -1;
+        } else {
+            return entry.getRank();
+        }
+
     }
 }
--- src/test/java/cloud/javacoder/bbcnewsgems/dictionary/DictionaryTest.java
+++ src/test/java/cloud/javacoder/bbcnewsgems/dictionary/DictionaryTest.java
@@ -1,28 +1,31 @@
 package cloud.javacoder.bbcnewsgems.dictionary;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest(classes = {Dictionary.class})
 public class DictionaryTest {
 
-    private DictionaryEntry dictionaryEntry;
+    private static DictionaryEntry entry_1 = new DictionaryEntry();
+    private Dictionary dictionary = new Dictionary();
 
     @BeforeAll
-    private void createEntry() {
-        dictionaryEntry.setRank(1);
-        dictionaryEntry.setWord("the");
-        dictionaryEntry.setPartOfSpeech("a");
-        dictionaryEntry.setFrequency(22038615);
-        dictionaryEntry.setDispersion(0.98F);
+    private static void createEntry() {
+        entry_1.setRank(1);
+        entry_1.setWord("the");
+        entry_1.setPartOfSpeech("a");
+        entry_1.setFrequency(22038615);
+        entry_1.setDispersion(0.98F);
     }
 
     @Test
     public void whenAddEntry_thenOK() {
-        Dictionary dictionary = new Dictionary();
-        dictionary.addEntry(dictionaryEntry);
+        log.info("About to add an entry: ");
+        dictionary.addEntry(entry_1);
 
         Assertions.assertThat(dictionary.getEntries().size()).isEqualTo(1);
     }
@@ -30,11 +33,33 @@ public class DictionaryTest {
     @Test
     public void dictionaryEntryHasRequiredFields() {
 
-        Assertions.assertThat(dictionaryEntry.getRank()).isEqualTo(1);
-        Assertions.assertThat(dictionaryEntry.getWord()).isEqualTo("the");
-        Assertions.assertThat(dictionaryEntry.getPartOfSpeech()).isEqualTo("a");
-        Assertions.assertThat(dictionaryEntry.getFrequency()).isEqualTo(22038615);
-        Assertions.assertThat(dictionaryEntry.getDispersion()).isEqualTo(0.98F);
+        Assertions.assertThat(entry_1.getRank()).isEqualTo(1);
+        Assertions.assertThat(entry_1.getWord()).isEqualTo("the");
+        Assertions.assertThat(entry_1.getPartOfSpeech()).isEqualTo("a");
+        Assertions.assertThat(entry_1.getFrequency()).isEqualTo(22038615);
+        Assertions.assertThat(entry_1.getDispersion()).isEqualTo(0.98F);
+    }
+
+    @Test
+    public void givenDictionaryContainsWord_returnsCorrectRank() {
+
+        dictionary.addEntry(entry_1);
+
+        DictionaryEntry entry_2 = new DictionaryEntry();
+        entry_2.setRank(2);
+        entry_2.setWord("be");
+        dictionary.addEntry(entry_2);
+
+        Assertions.assertThat(this.dictionary.containsWord("the")).isEqualTo(1);
+        Assertions.assertThat(this.dictionary.containsWord("be")).isEqualTo(2);
+    }
+
+    @Test
+    public void givenDictionaryDoesntContainWord_returnsNegative() {
+
+        dictionary.addEntry(entry_1);
+
+        Assertions.assertThat(this.dictionary.containsWord("notindictionary")).isEqualTo(-1);
     }
 
 }
